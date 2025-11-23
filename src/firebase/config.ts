d28d1a9e9b96f415bb43a32c3cd18a38a0ec42f0
Oli Snygg <oli.snygg@gmail.com>
2025-11-23 21:19:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7974049164-9ac0a",
+  "appId": "1:551021529948:web:acb4582b55bdc73acf4f99",
+  "apiKey": "AIzaSyDR_qmYR-aoFBWBDd-3SL7G6gQHmTtNRqE",
+  "authDomain": "studio-7974049164-9ac0a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "551021529948"
+};
